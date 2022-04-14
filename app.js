daccd534fc83951c156678ee97d72fdf29c9d321
Ramon Aurelio <donramonaurelio@gmail.com>
2022-04-14 10:43:53 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const hambuguer = document.querySelector(
+  ".header .nav-bar .nav-list .hamburguer"
+);
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(
+  ".header .nav-bar .nav-list ul li a"
+);
+
+const header = document.querySelector(".header.container");
+
+hambuguer.addEventListener("click", () => {
+  hambuguer.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  var scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    hambuguer.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
+});
